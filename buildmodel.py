fe0ab9a6aa9eec3035e27178d159024eb288951f
thq-quan <tahongquanyttq@gmail.com>
2022-12-28 15:59:22 +0700

    update buildmodel

--- buildmodel.py
+++ buildmodel.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import category_encoders as ce
+import tkinter
+import matplotlib
+matplotlib.use('Agg')
+from sklearn import metrics
 
 data = pd.read_csv("D:\doantotnghiep\datairender.csv")
 X = data.drop(['is_paid'], axis=1)
@@ -21,6 +25,10 @@ model_LR.fit(X_train,y_train)
 y_pred_LR = model_LR.predict(X_test)
 print('LogisticRegression')
 print_score(y_test, y_pred_LR)
+confusion_matrix = metrics.confusion_matrix(y_test, y_pred_LR)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+cm_display.plot()
+plt.savefig('bieudoLR.png')
 
 from sklearn import svm
 model_SVM = svm.SVC(kernel="linear", class_weight='balanced')
@@ -28,6 +36,10 @@ model_SVM.fit(X_train,y_train)
 y_pred_SVM = model_SVM.predict(X_test)
 print('svm')
 print_score(y_test, y_pred_SVM)
+confusion_matrix = metrics.confusion_matrix(y_test, y_pred_SVM)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+cm_display.plot()
+plt.savefig('bieudoSVM.png')
 
 from sklearn.neighbors import KNeighborsClassifier
 model_kNN = KNeighborsClassifier(n_neighbors=2)
@@ -35,6 +47,10 @@ model_kNN.fit(X_train,y_train)
 y_pred_kNN = model_kNN.predict(X_test)
 print('KNeighborsClassifier')
 print_score(y_test, y_pred_kNN)
+confusion_matrix = metrics.confusion_matrix(y_test, y_pred_kNN)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+cm_display.plot()
+plt.savefig('bieudoknn.png')
 
 from sklearn.tree import DecisionTreeRegressor
 model_Tree = DecisionTreeRegressor(random_state=0)
@@ -42,8 +58,12 @@ model_Tree.fit(X_train,y_train)
 y_pred_Tree = model_Tree.predict(X_test)
 print('DecisionTreeRegressor')
 print_score(y_test, y_pred_Tree.round())
+confusion_matrix = metrics.confusion_matrix(y_test, y_pred_Tree.round())
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+cm_display.plot()
+plt.savefig('bieudotree.png')
 
 print('Data correlation between two classes')
 print(data['is_paid'].value_counts())
 data['is_paid'].value_counts().plot(kind='bar')
-plt.show()
+plt.savefig('ssdata.png')
